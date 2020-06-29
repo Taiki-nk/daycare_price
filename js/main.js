20af49ab85f1412eeb7d4fa55dadde5cc2e9e78e
Taiki-nk <taikifate.0206@gmail.com>
2020-06-29 23:54:42 +0900

    基本的な機能は概ね実装完了

--- js/main.js
+++ js/main.js
@@ -1,24 +1,81 @@
-const carePrice = {
-  careLevel1: {
-    shortTime: 332,
-    longTime: 629,
-  },
-  careLevel2: {
-    shortTime: 386,
-    longTime: 754,
-  },
-  careLevel3: {
-    shortTime: 439,
-    longTime: 874,
-  },
-  careLevel4: {
-    shortTime: 493,
-    longTime: 1019,
-  },
-  careLevel5: {
-    shortTime: 547,
-    longTime: 1161,
-  },
-};
+"use strict";
+{
 
-console.log(carePrice.careLevel1.shortTime);
+  const carePrice = {
+    careLevel1: {
+      short: 332,
+      long: 629,
+    },
+    careLevel2: {
+      short: 386,
+      long: 754,
+    },
+    careLevel3: {
+      short: 439,
+      long: 874,
+    },
+    careLevel4: {
+      short: 493,
+      long: 1019,
+    },
+    careLevel5: {
+      short: 547,
+      long: 1161,
+    },
+  };
+
+  const btn = document.getElementById('btn');
+  const answer = document.getElementById('answer');
+  const careLevel = document.getElementById('careLevel');
+  const proportion = document.getElementById('proportion');
+  const time = document.getElementById('time');
+  const frequency = document.getElementById('frequency');
+  let price = 0;
+
+
+function calculatePrice(){
+  price =  carePrice[careLevel.value][time.value]
+  switch(frequency.value){
+    case 'fre1':
+      price *= 4;
+      break;
+    case 'fre2':
+      price *= 8;
+      break;
+    case 'fre3':
+      price *= 12;
+      break;
+    case 'fre4':
+      price *= 16;
+      break;
+    case 'fre5':
+      price *= 20;
+      break;
+    case 'fre6':
+      price *= 24;
+      break;
+    case 'fre7':
+      price *= 28;
+      break;
+  }
+
+  if(proportion.value === 'proportion2'){
+    price *= 2;
+  }
+
+  console.log(price)
+  showPrice();
+}
+
+function showPrice(){
+  answer.textContent = price;
+}
+
+
+  btn.addEventListener('click', () => {
+    
+    calculatePrice()
+    
+  });
+  
+}
