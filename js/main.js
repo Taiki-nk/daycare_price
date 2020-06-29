2f80b2ff30a836359eb519a644d2d7580105f484
Taiki-nk <taikifate.0206@gmail.com>
2020-06-29 22:47:53 +0900

    デイケア利用料金計算アプリの雛形作成

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const carePrice = {
+  careLevel1: {
+    shortTime: 332,
+    longTime: 629,
+  },
+  careLevel2: {
+    shortTime: 386,
+    longTime: 754,
+  },
+  careLevel3: {
+    shortTime: 439,
+    longTime: 874,
+  },
+  careLevel4: {
+    shortTime: 493,
+    longTime: 1019,
+  },
+  careLevel5: {
+    shortTime: 547,
+    longTime: 1161,
+  },
+};
+
+console.log(carePrice.careLevel1.shortTime);
